6df1a9c46e198147c09cbab7bcec6be5d2ddc55d
Eugene Evdokimenko <evdokimenko@tochka.com>
2024-07-05 13:01:44 +0400

    mid-level-step-objects  - upd scroll func

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,6 @@ from selenium import webdriver
 @pytest.fixture(scope = "function", autouse = True)
 def browser_manager():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_height = '1080'
-    browser.config.window_width = '1920'
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     driver_options.add_argument("--start-maximized")
