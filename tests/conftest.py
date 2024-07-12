aaede5bedf267c396db2e750b530239df45b069a
Eugene Evdokimenko <evdokimenko@tochka.com>
2024-07-12 23:18:21 +0400

    prepare for jenkins

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,19 @@
 import pytest
 from selene import browser
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from dotenv import load_dotenv
+import os
+
+
+@pytest.fixture(scope = "session", autouse = True)
+def load_env():
+    load_dotenv()
+
+
+selenoid_login = os.getenv("SELENOID_LOGIN")
+selenoid_pass = os.getenv("SELENOID_PASS")
+selenoid_url = os.getenv("SELENOID_URL")
 
 
 @pytest.fixture(scope = "function", autouse = True)
@@ -11,5 +24,21 @@ def browser_manager():
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor = f"https://{selenoid_login}:{selenoid_pass}@{selenoid_url}/wd/hub",
+        options = options)
+
+    browser.config.driver = driver
     yield
     browser.quit()
--- tests/test_demoqa_form.py
+++ tests/test_demoqa_form.py
@@ -1,10 +1,19 @@
+import allure
+from allure_commons.types import Severity
+
 import paths
 from modules.pages.registration_form_page import StudentRegistrationForm
 from modules.users import User
 
-
+@allure.tag("UI")
+@allure.severity(Severity.NORMAL)
+@allure.label("owner", "Evdokimenko")
+@allure.feature("Форма регистрации")
+@allure.story("Заполнение формы регистрации demoqa")
+@allure.link("https://demoqa.com", name="Testing")
 def test_practice_form_create_user():
-    registration_form = StudentRegistrationForm()
+    with allure.step('Открыть форму регистрации https://demoqa.com/automation-practice-form'):
+        registration_form = StudentRegistrationForm()
     user = User(name = 'Eugene', last_name = 'Tester', email = 'test@test.qom',
                 gender = 'Male',
                 phone = '7999321123',
@@ -17,5 +26,7 @@ def test_practice_form_create_user():
                 state = 'Haryana',
                 city = 'Panipat',
                 photo = paths.file_name)
-    registration_form.register(user)
-    registration_form.assert_user_should_have_registered(user)
+    with allure.step('Заполнить данные по пользователю и подтвердить регистрацию'):
+        registration_form.register(user)
+    with allure.step('ASSERT: Введенные данные пользователя отображаются в таблице'):
+        registration_form.assert_user_should_have_registered(user)
