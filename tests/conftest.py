4f5344e07abbc6f057a4c8e7718c12cb03536ed3
Eugene Evdokimenko <evdokimenko@tochka.com>
2024-07-13 18:14:15 +0400

    update env fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+import os
 import pytest
 from selene import browser
 from selenium import webdriver
@@ -6,27 +7,27 @@ from dotenv import load_dotenv
 from utils import attach
 
 
-@pytest.fixture(scope = "session", autouse = True)
+@pytest.fixture(scope = "function", autouse = True)
 def load_env():
     load_dotenv()
-
-#Todo разобраться с енвами - без них тест работает, также надо дописать шаги со скриншотами и логами
-# selenoid_login = os.getenv("SELENOID_LOGIN")
-# selenoid_pass = os.getenv("SELENOID_PASS")
-# selenoid_url = os.getenv("SELENOID_URL")
-selenoid_login = 'user1'
-selenoid_pass = '1234'
-selenoid_url = 'selenoid.autotests.cloud'
+    selenoid_login = os.getenv("SELENOID_LOGIN")
+    selenoid_pass = os.getenv("SELENOID_PASS")
+    selenoid_url = os.getenv("SELENOID_URL")
+    yield selenoid_login, selenoid_pass, selenoid_url
 
 
 @pytest.fixture(scope = "function", autouse = True)
-def browser_manager():
+def browser_manager(load_env):
+    selenoid_login, selenoid_pass, selenoid_url = load_env
+
     browser.config.base_url = 'https://demoqa.com'
     browser.config.window_height = '1080'
     browser.config.window_width = '1920'
+
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
+
     options = Options()
     selenoid_capabilities = {
         "browserName": "chrome",
@@ -46,7 +47,7 @@ def browser_manager():
 
     attach.add_logs(browser)
     attach.add_screenshot(browser)
-    attach.add_video(browser)
+    attach.add_video(browser, selenoid_url)
 
     yield
     browser.quit()
--- utils/attach.py
+++ utils/attach.py
@@ -1,6 +1,5 @@
 import allure
 from allure_commons.types import AttachmentType
-from tests.conftest import selenoid_url
 
 
 def add_screenshot(browser):
@@ -18,8 +17,8 @@ def add_html(browser):
     allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
 
 
-def add_video(browser):
-    video_url = f"{selenoid_url}/video/" + browser.driver.session_id + ".mp4"
+def add_video(browser, selenoid_url):
+    video_url = f"https://{selenoid_url}/video/" + browser.driver.session_id + ".mp4"
     html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
            + video_url \
            + "' type='video/mp4'></video></body></html>"
